e257cd15d9fd4cd423d3a01c05a66f6e47425cc5
Kabita <114086090+Kabita79@users.noreply.github.com>
2024-03-27 20:32:55 +0545

    rout.js

--- src/rout.js
+++ src/rout.js
@@ -11,8 +11,8 @@ const Rout = ({product, setProduct, detail, view, close, setClose, cart, setCart
   return (
     <>
     <Routes>
-        <Route path='/' element={<Home detail={detail} view={view} close={close} setClose={setClose} addtocart={addtocart} />}/>
-        <Route path='/product' element={<Product product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} addtocart={addtocart} />}/>
+        <Route path='/' element={<Home detail={detail} view={view} close={close} setClose={setClose} addtocart={addtocart} searchbtn={searchbtn} />}/>
+        <Route path='/product' element={<Product product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} addtocart={addtocart} searchbtn={searchbtn} />}/>
         <Route path='/cart' element={<Cart cart={cart} setCart={setCart} />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/footer' element={<Footer />} />
